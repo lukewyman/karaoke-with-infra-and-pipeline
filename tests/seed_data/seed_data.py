4ac17327e783fa60bec3bff7f88bf41ae7741575
Luke Wyman <c-luke.wyman@charter.com>
2023-03-13 22:37:27 -0600

    Workflow: fixed seed_data.json file path

--- tests/seed_data/seed_data.py
+++ tests/seed_data/seed_data.py
@@ -10,6 +10,13 @@ MONGODB_URL = "mongodb://admin:password1@0.0.0.0:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 db = client.song_library
 
-with open('seed_songs.json') as seed_file:
-    songs = json.load(seed_file)
-    db["song_library"].insert_many(songs)
+def main():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    full_path = os.path.join(dir_path, 'seed_songs.json')
+    with open(full_path) as seed_file:
+        songs = json.load(seed_file)
+        db["song_library"].insert_many(songs)
+
+
+if __name__ == "__main__": 
+    main()
